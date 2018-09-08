4e7b0060687df1b9fa7dac408c460eb9e2c7c309
Zane <github@zane.sh>
2018-09-07 23:29:20 -0400

    Fix link

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Flex, Container, Link } from '@hackclub/design-system'
+import { Flex, Container } from '@hackclub/design-system'
 
 const Base = Container.withComponent(Flex)
 
 export default () => (
   <Base justify="space-between" wrap w={1} maxWidth={48} px={3} pb={5}>
-    <a href="https://lachlanjc.me" target="_blank">
+    <a href="https://pennapps-xviii.devpost.com" target="_blank">
       Made at PennApps XVIII
     </a>
     <a className="github" href="https://github.com/lachlanjc/onevote">
