e6d0ce46c8bf83ba39e983fa089df288dade1bd1
Ava Scherocman <ava@scherocman.com>
2018-09-09 02:51:23 -0400

    Work on /positions endpoint

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const axios = require('axios')
 const express = require('express')
 const next = require('next')
 const scrapeIt = require('scrape-it')
-const fs = require('fs')
+const _ = require('lodash')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-
-const electorFile = JSON.parse(fs.readFileSync('data/electorData.json', 'utf8'))
 
 app.prepare().then(() => {
   const server = express()
@@ -36,31 +34,38 @@ app.prepare().then(() => {
   })
 
   server.get('/positions', (req, res) => {
-    const { name, state } = req.query
-    const baseUrl = 'https://votesmart.org/candidate/political-courage-test/'
-    electorFile.forEach(element => {
-      if (`${element.first_name} ${element.last_name}` === name) {
-        const vsUrl = `${baseUrl}${element.votesmart_candidate_id}/${name}/`
-        res.json({ url: vsUrl })
-      }
-    })
-
-    if (name && name !== '') {
-      scrapeIt(vsUrl, { data: 'tr .question-answer' }).then(
-        //use a list instead of regular data
-        ({ data, response }) => {
-          res.json(data)
-        }
-      )
-    } else {
-      res.status(400).json({ error: 'no name specified' })
+    const { rep, state } = req.query
+    if (!name || name === '' || !rep || rep === '') {
+      res
+        .status(400)
+        .json({ error: 'invalid parameters, name and rep are required.' })
     }
-  })
+    axios
+      .get(`https://votesmart.org/x/search?s=${encodeURIComponent(rep)}`)
+      .then(response => {
+        response.data.results.forEach(element => {
+          if (state === element.state_id && element.incumbent !== true) {
+            const baseUrl =
+              'https://votesmart.org/candidate/political-courage-test/'
+            const vsUrl = `${baseUrl}${
+              element.votesmart_candidate_id
+            }/${_.kebabCase(element.name)}`
+            /*scrapeIt(vsUrl, {
+              title: '.span-12 b',
+              desc: '.span-3'
+            }).then(({ data, response }) => {
+              console.log(`Status Code: ${response.statusCode}`)
+              console.log(data)
+            })*/
+          }
+        })
 
-  server.get('*', (req, res) => handle(req, res))
+        server.get('*', (req, res) => handle(req, res))
 
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+        server.listen(port, err => {
+          if (err) throw err
+          console.log(`> Ready on http://localhost:${port}`)
+        })
+      })
   })
 })
