e1384c86719d40e75e4457e2056d0391da77c991
Lachlan Campbell <lachlan@hackclub.com>
2018-09-10 17:14:21 -0400

    Update footer links

--- components/footer.js
+++ components/footer.js
@@ -8,13 +8,13 @@ export default () => (
   <Base justify="space-between" wrap width={1} maxWidth={48} px={3} pb={5}>
     <Link
       color="muted"
-      href="https://pennapps-xviii.devpost.com"
+      href="http://devpost.com/software/onevote"
       target="_blank"
     >
       Made at PennApps XVIII
     </Link>
     <Link color="muted" href="https://github.com/lachlanjc/onevote">
-      GitHub
+      Open source with ❤
     </Link>
   </Base>
 )
