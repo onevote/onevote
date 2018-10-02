8562ac46df565d82e6798bb29a46a71acf1d7683
Ava Scherocman <ava@scherocman.com>
2018-10-02 15:09:58 -0400

    Lachlan cares too much about typography

--- components/phoneSignup.js
+++ components/phoneSignup.js
@@ -30,7 +30,7 @@ const Location = ({ address }) => (
       <LargeButton bg={theme.colors.brand}>Sign up</LargeButton>
     </form>
     <Text mt={3} fontSize={1} color={theme.colors.muted}>
-      We'll never share or sell your data.
+      We’ll never share or sell your data.
     </Text>
   </Base>
 )
