290e91e46784c453d4c0a1258c13ad53f4374735
Ava Scherocman <ava@scherocman.com>
2018-10-02 15:01:00 -0400

    Change placeholder phone number
    Dalton looked over my shoulder and told me to change it to the right number of digits

--- components/phoneSignup.js
+++ components/phoneSignup.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import theme from '../theme/config'
-import { Heading, Box, Field, LargeButton } from '@hackclub/design-system'
+import { Heading, Box, Text, Field, LargeButton } from '@hackclub/design-system'
 
 const Base = styled(Box)`
   position: relative;
@@ -17,18 +17,21 @@ Base.defaultProps = {
 
 const Location = ({ address }) => (
   <Base my={4}>
-    <Heading.h2 mb={2} style={{ fontWeight: 'bold' }}>
+    <Heading.h2 mb={3} style={{ fontWeight: 'bold' }}>
       Get reminded before the election
     </Heading.h2>
     <form>
       <Field
         label="Your phone number"
-        placeholder="(123) 456-789"
+        placeholder="(123) 456-7890"
         name="phone"
         mb={3}
       />
       <LargeButton bg={theme.colors.brand}>Sign up</LargeButton>
     </form>
+    <Text mt={3} fontSize={1} color={theme.colors.muted}>
+      We'll never share or sell your data.
+    </Text>
   </Base>
 )
 
